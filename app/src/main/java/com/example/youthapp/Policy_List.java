c0ad4e966897ebad5e9f5ea924305d21b1ecf7e1
KIMTAEWON <guineapig7351@gmail.com>
2022-06-02 23:38:31 +0900

    필터

--- app/src/main/java/com/example/youthapp/Policy_List.java
+++ app/src/main/java/com/example/youthapp/Policy_List.java
@@ -3,28 +3,59 @@ package com.example.youthapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.transition.Fade;
+import android.transition.Slide;
+import android.transition.Transition;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class Policy_List extends AppCompatActivity {
 
+    TextView policy_fillter;
+    Spinner spinnerPolicy, spinnerLocal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_policy_list);
 
+        spinnerPolicy = findViewById(R.id.spinnerPolicy);
+        spinnerLocal = findViewById(R.id.spinnerLocal);
+        policy_fillter = findViewById(R.id.fillter);
+
+
+        policy_fillter.setOnClickListener(new View.OnClickListener() {
+            boolean show = false;
+            @Override
+            public void onClick(View view) {
+
+                if (show == true)
+                {
+                    spinnerLocal.setVisibility(view.GONE);
+                    spinnerPolicy.setVisibility(view.GONE);
+                    show = false;
+                }
+                else
+                {
+                    spinnerLocal.setVisibility(view.VISIBLE);
+                    spinnerPolicy.setVisibility(view.VISIBLE);
+                    show = true;
+                }
+            }
+        });
+
         String[] Data1 = {"유형 전체","취업", "창업", "주거ㆍ금융", "생활ㆍ복지", "정책참여"};
         String[] Data2 = {"지역 전체","중앙부처","서울","부산","대구","인천","광주","대전","울산","경기","강원",
                 "충북","충남","전북","전남","경북","경남","제주","세종"};
-        Spinner spinnerPolicy = findViewById(R.id.spinnerPolicy);
-        Spinner spinnerLocal = findViewById(R.id.spinnerLocal);
+
 
         //정책 유형 선택
         ArrayAdapter<String> policyAdapter = new ArrayAdapter<>(getApplicationContext(),
@@ -32,13 +63,19 @@ public class Policy_List extends AppCompatActivity {
         policyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         spinnerPolicy.setAdapter(policyAdapter);
 
+
         // 지역 선택
         ArrayAdapter<String> localAdapter = new ArrayAdapter<>(getApplicationContext(),
                 android.R.layout.simple_spinner_item,Data2);
         policyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         spinnerLocal.setAdapter(localAdapter);
 
+        //스피너 애니메이션
+        Transition localTransition = new Fade();
+        localTransition.addTarget(spinnerLocal);
 
+        Transition policyTransition = new Fade();
+        policyTransition.addTarget(spinnerPolicy);
 
 
         ArrayList<String> policyData = new ArrayList<>();
